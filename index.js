725f044796c9a105d3e7c00b399208b3a103b801
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-13 19:58:52 +0530

    signup user with post api has been created

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const app = express();
 const PORT = 3001;
 const { connectDB } = require("./config/db");
 const userRoutes = require("./routes/user")
-//middleware
-app.use(express.json());
-app.use(express.static("content"));
 
-app.use("api/v1/user", userRoutes)
+connectDB();
+//middleware
+app.use(express.json())
+app.use(express.static("content"))
+app.use(express.urlencoded({extended: false}))
+
+
+app.use("/api/v1/user", userRoutes)
 
 app.listen(PORT, () => {
   console.log("server is running at", PORT);
-  connectDB();
 });
--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,65 @@
-const express = require("express")
-
-const router = express.Router()
+const express = require("express");
+console.log("reached here");
+const brcypt = require("bcrypt");
+const router = express.Router();
+const User = require("../models/userModels");
 const {
-    validatName,
-    validatEmail,
-    validatePassword
-} = require("../utlis/validators")
+  validatName,
+  validatEmail,
+  validatePassword,
+  validateEmail,
+  validateName,
+} = require("../utlis/validators");
 
-router.post("/signup", async(req, res)=>{
-    try {
-        const {name, email, password, isSeller} = req.body;
+router.post("/signup", async (req, res) => {
+  try {
+    console.log("made to try");
+    const { name, email, password, isSeller } = req.body;
 
-        const existigUser = await 
-    } catch (error) {
-        return res.status(500).send(error)
+    const existigUser = await User.findOne({
+      where: { email: email },
+    });
+    if (existigUser) {
+      return res.status(403).json({ err: "User already Exits" });
     }
-})
+    if (!validateName(name)) {
+      return res.status(400).json({ err: "Name validate fails" });
+    }
+    if (!validateEmail(email)) {
+      return res.status(400).json({ err: "email validate fails" });
+    }
+    if (!validatePassword(password)) {
+      return res.status(400).json({ err: "Password validate fails" });
+    }
+    const hashedPassword = await brcypt.hash(password, (saltOrRounds = 10));
+
+    const user = {
+      email,
+      name,
+      password: hashedPassword,
+      isSeller: isSeller || false,
+    };
+
+    const createdUser = await User.create(user);
+
+    console.log(createdUser);
+    return res.status(201).json({
+      message: `welcome ${createdUser.name}`,
+    });
+  } catch (error) {
+    console.log("made to catch");
+    return res.status(500).send(error);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  try {
+    const { name } = req.body;
+    return res.status(200).json({ result: name });
+  } catch (error) {
+    console.log("kdjfka");
+    return res.status(500).send(error);
+  }
+});
 
 module.exports = router;
